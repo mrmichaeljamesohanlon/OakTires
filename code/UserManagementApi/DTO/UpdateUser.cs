a261ab069971f49cf6eb532491547cfa169ec859
Your Name <you@example.com>
2025-10-29 18:44:43 +0000

    missing artefacts

---
+++ code/UserManagementApi/DTO/UpdateUser.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementApi.DTO
+{
+    public class UpdateUserDto
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
---
+++ code/UserManagementApi/DTO/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace UserManagementApi.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+    }
+
+}
